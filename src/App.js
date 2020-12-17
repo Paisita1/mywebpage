542d219a8e0cec0f9116073683fdd27e1315c344
Paisita1 <isaacvirgen123@gmail.com>
2020-12-17 13:53:53 -0800

    Working on homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  useHistory
+} from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {};
+
+export default class MyWebsite extends React.Component {
+  render() {
+    return (
+        <Homepage />
+
+      )
+  }
 }
 
-export default App;
+class Title extends React.Component {
+  render() {
+    return (
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <h1>{'Welcome to my project showcase!'}</h1>
+          <h4 style={{textAlign: "center"}}>{'Please login below'}</h4>
+        </div>
+      );
+  }
+}
+
+class Login extends React.Component {
+  render() {
+    return (
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <input className="login" placeholder="Username" style={styles.login} />
+          <input type="password" placeholder="Password" />
+          <input type="button" value="Submit" />
+        </div>
+      )
+  }
+}
+
+styles.login = {
+  fontSize: "17px",
+  borderRadius: "6px",
+  padding: "14px 16px",
+  width: "330px",
+  border: "1px solid #dddfe2",
+  height: "22px"
+}
+
+class Homepage extends React.Component {
+  render() {
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+          <Title />
+          <Login />
+        </div>
+      );
+  }
+}
